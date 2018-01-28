399b96839ff277dbff884402a39f91d7b8a0044d
Andreas Fieger <fiedsch@ja-eh.at>
2018-01-28 10:30:54 +0100

    refactore code

--- src/Manager.php
+++ src/Manager.php
@@ -8,14 +8,12 @@ use PHPUnit\Framework\MockObject\RuntimeException;
 
 /**
  * Class Manager
+ * A Helper class for working with SQLite Databases.
  *
  * @package Fiedsch\SqliteManager
- *
- *
  */
 class Manager
 {
-
     /**
      * @var string ID_COLUMN_NAME Name of the id column
      */
@@ -42,9 +40,9 @@ class Manager
     /**
      * Connect to a (file based) SQLite Database
      *
-     * @param string $dbpath
-     * @param boolean $dbMustExist
-     * @throws \RuntimeException
+     * @param string $dbpath Filepath of the SQLite database
+     * @param boolean $dbMustExist specifies wether or not the $dbpath is expected to exist
+     * @throws \RuntimeException if $dbMustExist is true and the file at $dbpath does not exist
      * @throws \Doctrine\DBAL\DBALException
      * @return \Doctrine\DBAL\Connection
      */
@@ -54,7 +52,7 @@ class Manager
             'driver' => 'pdo_sqlite',
             'path'   => $dbpath,
         ];
-        if ($dbMustExist && !$this->assertDbPath($dbpath)) {
+        if ($dbMustExist && !$this->assertDbFile($dbpath)) {
             throw new \RuntimeException("'$dbpath' does not exist");
         }
         $config = new Configuration();
@@ -64,37 +62,145 @@ class Manager
     /**
      * Assert that the specified file---a SQLIte database---exists.
      * TODO: we might also want to test if an existing file is a SQLite database
+     * maybe use file foo.db => "foo.db: SQLite 3.x database, ..."
      *
-     * @param string $dbpath
+     * @param string $dbpath Filepath of the SQLite database
      * @return boolean
      */
-    public function assertDbPath($dbpath)
+    public function assertDbFile($dbpath)
     {
         return file_exists($dbpath);
     }
 
     /**
-     * Create the "CREATE TABLE" SQL for $table which's columns are described be $colspec
+     * Create the "CREATE TABLE" SQL for $table which's columns are described
+     * by $colspec
      *
-     * TODO: allow 'NOT NULL' constraint
      * TODO: allow multicolumn UNIQUE constraints
      *
      * @param string $tablename Name of the database table
      * @param array $colspec Specification of the table's columns (name and data type)
      * @return string SQL to create the table
      */
-    public function getCreateTableSql($tablename, $colspec)
+    public function getCreateTableSql($tablename, array $colspec)
     {
         $idcolumn = self::ID_COLUMN_NAME . ' ' . self::ID_COLUMN_TYPE;
         $columns = $this->getColSpecsSql($colspec);
         $constraints = $this->getUniqueConstraintsSql($colspec);
         return sprintf("CREATE TABLE %s %s (%s%s%s)",
-            'IF NOT EXISTS', // TODO optional
+            'IF NOT EXISTS', // TODO make optional
             $tablename,
             $idcolumn,
             $columns ? ",$columns" : '',
             $constraints ? ",$constraints" : ''
         );
+    }
+
+    /**
+     * Get the SQL for the table's columns as specified in $colspecs
+     *
+     * @param array $colspecs columns specifications
+     * @throws \RuntimeException
+     * @return string
+     */
+    protected function getColSpecsSql(array $colspecs)
+    {
+        $result = [];
+        foreach ($colspecs as $colname => $colconfig) {
+            if (self::ID_COLUMN_NAME === $colname) {
+                throw new \RuntimeException("column name '$colname' is reserved for internal usage");
+            }
+            $result[] = $this->getColSql($colname, $colconfig);
+        }
+        return join(',', $result);
+    }
+
+    /**
+     * Get the SQL for the column $colname as specified in $colsconfig
+     *
+     * @param string $colname The column's name
+     * @param array $colconfig The column's specification
+     * @throws \RuntimeException if the configuration is not valid
+     * @return null|string|string[]
+     */
+    protected function getColSql($colname, array $colconfig)
+    {
+        if (isset($colconfig['unique']) && true === $colconfig['unique']
+            && isset($colconfig['default']) && !is_null($colconfig['default'])
+        ) {
+            throw new \RuntimeException("you can not have a UNIQUE column with a DEFAULT value");
+        }
+        if (isset($colconfig['mandatory']) && true === $colconfig['mandatory']
+            && (!isset($colconfig['default']) || is_null($colconfig['default']))
+        ) {
+            throw new \RuntimeException("you can not have a NOT NULL column with default value NULL");
+        }
+        return preg_replace("/\s+/", ' ', trim(sprintf("%s %s %s %s %s",
+            $colname,
+            $this->getColtype($colconfig),
+            $this->getNullContraint($colconfig),
+            $this->getDefaultSetting($colconfig),
+            $this->getUniqueContraint($colconfig)
+        )));
+    }
+
+    /**
+     * Get the column type (SQLite "affinity") from the column's specification
+     * For supported column types see https://www.sqlite.org/datatype3.html
+     *
+     * @param array $colconfig The column's specification
+     * @throws \RuntimeException if the specified type is not valid
+     * @return string
+     */
+    protected function getColtype(array $colconfig)
+    {
+        $type = isset($colconfig['type']) ? $colconfig['type'] : 'column type not set';
+        if (in_array(strtoupper($type), ['INTEGER', 'TEXT', 'BLOB', 'REAL', 'NUMERIC'])) {
+            return $type;
+        }
+        throw new RuntimeException("column type '$type' is not supported'");
+    }
+
+    /**
+     * Adds 'NOT NULL' constraint if $colconfig['mandatory'] is true
+     *
+     * @param array $colconfig The column's specification
+     * @return string
+     */
+    protected function getNullContraint(array $colconfig)
+    {
+        if (isset($colconfig['mandatory']) && true === $colconfig['mandatory']) {
+            return 'NOT NULL';
+        }
+        return '';
+    }
+
+    /**
+     * 'DEFAULT ...' constraint if $colconfig['default'] is set, '' otherwise
+     *
+     * @param array $colconfig The column's specification
+     * @return string
+     */
+    protected function getDefaultSetting(array $colconfig)
+    {
+        if (isset($colconfig['default']) && !is_null($colconfig['default'])) {
+            return sprintf("DEFAULT '%s'", $colconfig['default']);
+        }
+        return '';
+    }
+
+    /**
+     * 'UNIQUE' constraint if $colconfig['unique'] is true, '' otherwise
+     *
+     * @param array $colconfig The column's specification
+     * @return string
+     */
+    protected function getUniqueContraint(array $colconfig)
+    {
+        if (isset($colconfig['unique']) && true === $colconfig['unique']) {
+            return 'UNIQUE';
+        }
+        return '';
     }
 
     /**
@@ -108,8 +214,11 @@ class Manager
      * @param array $colspecs Specifications for the columns
      * @return string
      */
-    protected function getUniqueConstraintsSql($colspecs)
+    protected function getUniqueConstraintsSql(array $colspecs)
     {
+        return '';
+        // TODO: for multicolumn constraints only!
+        /*
         return implode(',',
             array_map(
                 function($el) {
@@ -120,66 +229,32 @@ class Manager
                 }))
             )
         );
+        */
     }
 
     /**
-     * @param array $colspecs columns specifications
+     * Create the "ADD COLUMN ..." SQL for $table and $colname
+     *
+     * @param string $tablename Name of the database table
+     * @param string $colname Name of the column to be added
+     * @param array $colconfig Configuration for $colname
      * @throws \RuntimeException
-     * @return string
+     * @retunr string
      */
-    protected function getColSpecsSql($colspecs)
+    public function getAddColumnSql($tablename, $colname, array $colconfig)
     {
-        $result = [];
-        foreach ($colspecs as $colname => $colconfig) {
-            if (self::ID_COLUMN_NAME === $colname) {
-                throw new \RuntimeException("column name '$colname' is reserved for internal usage");
-            }
-            $result[] = trim(sprintf("%s %s %s %s",
-                $colname,
-                $this->checkColtype($colconfig),
-                $this->getNullContraint($colconfig),
-                $this->getUniqueContraint($colconfig)
-            ));
+        if (isset($colconfig['unique']) && true === $colconfig['unique']) {
+            throw new \RuntimeException("you can not add a UNIQUE column");
         }
-        return join(',', $result);
-    }
-
-    /**
-     * TODO: implement and document what is expected in $colconfig
-     *
-     * @param array $colconfig
-     * @return string
-     */
-    protected function getNullContraint($colconfig)
-    {
-        return ''; // TODO: 'NOT NULL' if ..., '' otherwise
-    }
-
-    /**
-     * TODO: implement and document what is expected in $colconfig
-     *
-     * @param array $colconfig
-     * @return string
-     */
-    protected function getUniqueContraint($colconfig)
-    {
-        return ''; // TODO
-    }
-
-    /**
-     * For supported column types see https://www.sqlite.org/datatype3.html
-     *
-     * @param array $colconfig
-     * @throws \RuntimeException
-     * @return string
-     */
-    protected function checkColtype($colconfig)
-    {
-        $type = isset($colconfig['type']) ? $colconfig['type'] : 'column type not set';
-        if (in_array(strtoupper($type), ['INTEGER', 'TEXT', 'BLOB', 'REAL', 'NUMERIC'])) {
-            return $type;
+        if (isset($colconfig['mandatory']) && true === $colconfig['mandatory']
+            && (!isset($colconfig['default']) || is_null($colconfig['default']))
+        ) {
+            throw new \RuntimeException("you can not add a NOT NULL column with default value NULL");
         }
-        throw new RuntimeException("column type '$type' is not supported'");
+        return sprintf('ALTER TABLE %s ADD COLUMN %s',
+            $tablename,
+            $this->getColSql($colname, $colconfig)
+        );
     }
 
 }
--- tests/ManagerTest.php
+++ tests/ManagerTest.php
@@ -19,26 +19,33 @@ class ManagerTest extends TestCase
      */
     protected $dbpath = __DIR__ . '/assets/test.db';
 
+    /**
+     * Make sure column specifications that make no sense
+     * causes an exception to be thrown.
+     */
     protected function setUp()
     {
         $this->manager = new Manager();
         register_shutdown_function(function() {
-            if(file_exists($this->dbpath)) {
+            if (file_exists($this->dbpath)) {
                 unlink($this->dbpath);
             }
         });
     }
 
+    /**
+     *
+     */
     protected function tearDown()
     {
         // nothing to do at the moment
     }
 
-    /*
-     * If we do not set the optional parameter to true connectTo()
+    /**
+     * If we do not set the optional parameter of connectTo() to true
      * the existance of the database file does will not be checked
      * and connecting to a SQLite database without performing any
-     * queries will not creaate the database file
+     * queries will not create the database file.
      */
     public function testGetConnection()
     {
@@ -46,9 +53,9 @@ class ManagerTest extends TestCase
         $this->assertInstanceOf(Connection::class, $connection);
     }
 
-    /*
-     * If we set the optional parameter to true connectTo() will throw
-     * a \RuntimeException if the database file does not exist.
+    /**
+     * Same as testGetConnection() but this time with check for
+     * existance of the database file--which does not exist.
      */
     public function testGetConnectionWithException()
     {
@@ -56,28 +63,65 @@ class ManagerTest extends TestCase
         $this->manager->connectTo($this->dbpath, Manager::DB_FILE_MUST_EXIST);
     }
 
-    /*
-     * This test creates a table in the database causing the SQLite
-     * database file to be finally created. The file will be removed
-     * in a shutdown function---see setUp().
+    /**
+     * As a side effect of this test the SQLite database file will be finally
+     * created. The file will be removed again in a shutdown function---see setUp().
+     *
+     * Tests that require this file to exist have to set '@ depends testCreateTable'!
      */
     public function testCreateTable()
     {
         $connection = $this->manager->connectTo($this->dbpath);
         $this->assertInstanceOf(Connection::class, $connection);
-        $this->assertFalse($this->manager->assertDbPath($this->dbpath));
-        $colspec = [
+        $this->assertFalse($this->manager->assertDbFile($this->dbpath));
+        $columns = [
             'bar' => [
-                    'type' => 'TEXT'
-            ]
+                'type' => 'TEXT',
+            ],
         ];
-        $expected = $this->manager->getCreateTableSql('foo', $colspec);
+        $expected = $this->manager->getCreateTableSql('foo', $columns);
         $connection->executeQuery($expected);
-        $this->assertTrue($this->manager->assertDbPath($this->dbpath));
+        $this->assertTrue($this->manager->assertDbFile($this->dbpath));
     }
 
     /**
-     * Invalid column type specifications shall throw an exception.
+     * The column specifications have to make sense.
+     */
+    public function testInvalidColumsSpecificationInCreateTable()
+    {
+        $columns = [
+            'bar' => [
+                'type'      => 'TEXT',
+                'mandatory' => true,
+                'default'   => '42',
+                'unique'    => true,
+            ],
+        ];
+        // you can not have a unique column with a default value
+        $this->expectException(\RuntimeException::class);
+        $this->manager->getCreateTableSql('foo', $columns);
+    }
+
+    /**
+     * The column specifications have to make sense (II).
+     */
+    public function testInvalidColumsSpecificationInCreateTablePart2()
+    {
+        $columns = [
+            'bar' => [
+                'type'      => 'TEXT',
+                'mandatory' => false,
+                'default'   => '42',
+                'unique'    => true,
+            ],
+        ];
+        $this->expectException(\RuntimeException::class);
+        $this->manager->getCreateTableSql('foo', $columns);
+    }
+
+    /**
+     * Invalid column type specifications have to cause an exception to be
+     * thrown.
      * We only allow 'INTEGER', 'TEXT', 'BLOB', 'REAL' or 'NUMERIC'
      * see https://www.sqlite.org/datatype3.html
      */
@@ -85,8 +129,8 @@ class ManagerTest extends TestCase
     {
         $colspec = [
             'foo' => [
-                'type' => 'VARCHAR(10)'
-            ]
+                'type' => 'VARCHAR(10)',
+            ],
         ];
         $this->expectException(\RuntimeException::class);
         $this->manager->getCreateTableSql('foo', $colspec);
@@ -100,30 +144,87 @@ class ManagerTest extends TestCase
     {
         $colspec = [
             'id' => [
-                'type' => 'TEXT'
-            ]
+                'type' => 'TEXT',
+            ],
         ];
         $this->expectException(\RuntimeException::class);
         $this->manager->getCreateTableSql('foo', $colspec);
     }
 
+    /**
+     * If we set the optional parameter to true connectTo() will throw
+     * a \RuntimeException if the database file does not exist.
+     */
     public function testGetCreateTableSql()
     {
         $tablename = 'foobar';
         $colspec = [
             'bar' => [
-                'type' => 'TEXT'
+                'type'   => 'TEXT',
+                'unique' => true,
             ],
             'baz' => [
-                'type' => 'INTEGER',
-                'unique' => true,
-            ]
+                'type'      => 'INTEGER',
+                'unique'    => false,
+                'mandatory' => true,
+                'default'   => 42
+             ],
         ];
-        $expected = 'CREATE TABLE IF NOT EXISTS foobar ';
-        $expected .= '(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,';
-        $expected .= 'bar TEXT,baz INTEGER';
-        $expected .= ',CONSTRAINT unique_baz_constraint UNIQUE (baz))';
+        $expected = 'CREATE TABLE IF NOT EXISTS foobar';
+        $expected .= ' (';
+        $expected .= 'id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT';
+        $expected .= ',bar TEXT UNIQUE';
+        $expected .= ",baz INTEGER NOT NULL DEFAULT '42'";
+        //$expected .= ',CONSTRAINT unique_barbaz_constraint UNIQUE (bar,baz)';
+        $expected .= ')';
         $this->assertEquals($expected, $this->manager->getCreateTableSql($tablename, $colspec));
+    }
+
+    /**
+     *
+     */
+    public function testGetAddColumnSql()
+    {
+        $colconfig = [
+            'type'      => 'REAL',
+            'mandatory' => true,
+            'default'   => '42',
+        ];
+        $sql = $this->manager->getAddColumnSql('tablename', 'colname', $colconfig);
+        $expected = "ALTER TABLE tablename ADD COLUMN colname REAL NOT NULL DEFAULT '42'";
+        $this->assertEquals($expected, $sql);
+
+        // you can not add a UNIQUE column
+        $colconfig['unique'] = true;
+        $this->expectException(\RuntimeException::class);
+        $this->manager->getAddColumnSql('tablename', 'colname', $colconfig);
+    }
+
+    /**
+     * Add a column with default value to the table and insert a row.
+     * Then check if we were successful.
+     *
+     * Make sure, these tests run before this one:
+     * @depends testCreateTable
+    */
+    public function testAddColumnSql()
+    {
+        $colconfig = [
+            'type'      => 'REAL',
+            'mandatory' => true,
+            'default'   => '42',
+        ];
+        $sql = $this->manager->getAddColumnSql('foo', 'newcol', $colconfig);
+        $connection = $this->manager->connectTo($this->dbpath);
+        $connection->executeUpdate($sql);
+        $connection->executeUpdate("INSERT INTO foo (bar) VALUES ('bar')");
+        $result = $connection->query("SELECT * FROM foo");
+        $expectedrow = [
+            'id'     => 1,
+            'bar'    => 'bar',
+            'newcol' => 42.0,
+        ];
+        $this->assertEquals($expectedrow, $result->fetch());
     }
 
 }
